68a27f3daeaea83f1788f86648f409e0139681cb
Jfrederick12 <jfrederickdesign@gmail.com>
2017-02-09 11:49:31 -0600

    add modal for each work

--- src/pages/Portfolio/ImageList.js
+++ src/pages/Portfolio/ImageList.js
@@ -1,33 +1,41 @@
 import React, { Component } from 'react';
 import ShowCard from './ShowCard';
 import './Portfolio.css';
+import { Modal, OverlayTrigger, popover, tooltip, Button } from 'react-bootstrap';
 
 class ImageList extends React.Component {
   constructor(props) {
     super(props);
     this.selectedWork = {id: null}
-    this.state = this.selectedWork;
-
+    this.state =  {
+      id: this.selectedWork.id,
+      name: this.selectedWork.name,
+      img: this.selectedWork.img,
+      showModal: false
+    }
+    console.log(this.state);
     this.count = 0
-
-    this.onCowClick = this.onCowClick.bind(this);
   }
 
+  close() {
+    this.setState({ showModal: false });
+  }
 
-  onCowClick(evt) {
-    this.setState({
-      num: this.state.num + 1
-    });
+  open() {
+    this.setState({ showModal: true });
+    console.log(this.state)
   }
 
   selectWork(id, name, img){
-    this.selectedWork = {id: id, name: name, img: img}
+    this.selectedWork = {id: id, name: name, img: img, showModal: true}
     this.state = this.selectedWork
-    console.log(this.selectedWork)
-    // this.state = {id: id + 1 , name: name}
+    // console.log(this.selectedWork)
     console.log(this.state.id)
+    console.log(this.state)
+    // console.log(this.showModal)
     this.setState({
-      id: this.state.id
+      id: this.state.id,
+      showModal: true
     });
   }
 
@@ -45,12 +53,35 @@ class ImageList extends React.Component {
       <ul className="work-container" >
         {works.map((work) => (
           <li className="flex-item" onMouseOver={this.onHoverOver} onMouseOut={this.onHoverOut} key={work.id} onClick={this.selectWork.bind(this, work.id, work.name, work.img )}>
-            <p onClick={this.onCowClick}>work id: {this.state.id}</p>
+            <p onClick={this.open}>work id: {this.state.id}</p>
 
             <ShowCard {...work} />
           </li>
         ))}
       <p><img src={this.state.img}/></p>
+
+      <Modal show={this.state.showModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>Text in a modal</h4>
+          <p><img src={this.state.img}/></p>
+
+          <h4>Popover in a modal</h4>
+          <hr />
+
+          <h4>Overflowing text to show scroll behavior</h4>
+          <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+          <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
+          <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button>Close</Button>
+        </Modal.Footer>
+      </Modal>
       </ul>
     );
   }
