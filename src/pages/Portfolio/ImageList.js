68275fd84886e2b09e2af3f49faa570c49c3875b
Jfrederick12 <jfrederickdesign@gmail.com>
2017-02-09 16:03:48 -0600

    update description for each item

--- src/pages/Portfolio/ImageList.js
+++ src/pages/Portfolio/ImageList.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import ShowCard from './ShowCard';
 import './Portfolio.css';
+// const projects = require('../../../public/data');
 import { Modal, OverlayTrigger, popover, tooltip, Button } from 'react-bootstrap';
+
+
+
 
 class ImageList extends React.Component {
   constructor(props) {
@@ -11,14 +15,19 @@ class ImageList extends React.Component {
       id: this.selectedWork.id,
       name: this.selectedWork.name,
       img: this.selectedWork.img,
+      description: this.selectedWork.description,
       showModal: false
     }
+    this.close = this.close.bind(this);
     console.log(this.state);
     this.count = 0
   }
 
   close() {
-    this.setState({ showModal: false });
+    this.setState({
+      id: this.state.id,
+      showModal: false
+    });
   }
 
   open() {
@@ -26,13 +35,12 @@ class ImageList extends React.Component {
     console.log(this.state)
   }
 
-  selectWork(id, name, img){
-    this.selectedWork = {id: id, name: name, img: img, showModal: true}
+  selectWork(id, name, img, description){
+    this.selectedWork = {id: id, name: name, img: img, description: description, showModal: true}
     this.state = this.selectedWork
-    // console.log(this.selectedWork)
     console.log(this.state.id)
     console.log(this.state)
-    // console.log(this.showModal)
+
     this.setState({
       id: this.state.id,
       showModal: true
@@ -52,68 +60,66 @@ class ImageList extends React.Component {
     return (
       <ul className="work-container" >
         {works.map((work) => (
-          <li className="flex-item" onMouseOver={this.onHoverOver} onMouseOut={this.onHoverOut} key={work.id} onClick={this.selectWork.bind(this, work.id, work.name, work.img )}>
-            <p onClick={this.open}>work id: {this.state.id}</p>
-
+          <li className="flex-item" onMouseOver={this.onHoverOver} onMouseOut={this.onHoverOut} key={work.id} onClick={this.selectWork.bind(this, work.id, work.name, work.img, work.description )}>
             <ShowCard {...work} />
           </li>
         ))}
-      <p><img src={this.state.img}/></p>
 
-      <Modal show={this.state.showModal}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <h4>Text in a modal</h4>
-          <p><img src={this.state.img}/></p>
+        {/* need to move Modal into it's own component and pass state through as props */}
 
-          <h4>Popover in a modal</h4>
-          <hr />
+        <Modal className='work-modal' show={this.state.showModal} onHide={this.close}>
+          <Modal.Header closeButton onClick={this.close}>
+          </Modal.Header>
+          <Modal.Body>
+            <p><img src={this.state.img} width="100%" /></p>
+            <h4>{this.state.name}</h4>
+            <hr />
+            <p>{this.state.description}</p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.close}>Close</Button>
+          </Modal.Footer>
+        </Modal>
 
-          <h4>Overflowing text to show scroll behavior</h4>
-          <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
-          <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
-          <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button>Close</Button>
-        </Modal.Footer>
-      </Modal>
       </ul>
     );
   }
 };
 
 
-
 const works = [
       { id: 1,
         name: "Moonphase Ranch",
-        img: "https://d13yacurqjgara.cloudfront.net/users/1110044/screenshots/2648085/moonphase.jpg"
+        img: "https://d13yacurqjgara.cloudfront.net/users/1110044/screenshots/2648085/moonphase.jpg",
+        description: "I spent a week on a ranch just outside of Austin, TX recording an upcoming record. Inspired by the acres of Texas hill country, a cozy air-stream trailer and free roaming cats, I created an illustration."
       },
       { id: 2,
         name: "CTA Illustration",
-        img: "https://d13yacurqjgara.cloudfront.net/users/1110044/screenshots/2616895/ctatrain.jpg"
+        img: "https://d13yacurqjgara.cloudfront.net/users/1110044/screenshots/2616895/ctatrain.jpg",
+        description: "A series of illustrations commisioned by Chicago Red Eye to do a series of illustrations based on Chicago landmarks for their Social Media."
       },
       { id: 3,
         name: "Little Boy Jr",
-        img: "https://d13yacurqjgara.cloudfront.net/users/1110044/screenshots/2633812/lbjdetail.jpg"
+        img: "https://d13yacurqjgara.cloudfront.net/users/1110044/screenshots/2633812/lbjdetail.jpg",
+        description: "Work in progress for album artwork."
       },
       { id: 4,
         name: "Music Box",
-        img: "https://d13yacurqjgara.cloudfront.net/users/1110044/screenshots/2615073/music_box.jpg"
+        img: "https://d13yacurqjgara.cloudfront.net/users/1110044/screenshots/2615073/music_box.jpg",
+        description: "A series of illustrations commisioned by Chicago Red Eye to do a series of illustrations based on Chicago landmarks for their Social Media."
       },
       { id: 5,
         name: "Tuff Slang",
-        img: "https://d13yacurqjgara.cloudfront.net/users/1110044/screenshots/2599356/tuffslangvinyl.jpg"
+        img: "https://d13yacurqjgara.cloudfront.net/users/1110044/screenshots/2599356/tuffslangvinyl.jpg",
+        description: 'Album artwork for Tuff Slang release "Tuff Slang".'
       },
       { id: 6,
         name: "Blonde Ventura",
-        img: "https://d13yacurqjgara.cloudfront.net/users/1110044/screenshots/2596141/bv_screen.jpg"
+        img: "https://d13yacurqjgara.cloudfront.net/users/1110044/screenshots/2596141/bv_screen.jpg",
+        description: 'Type treatment for Los Angeles based Disco Pop group "Blonde Ventura". The idea was to go for a disco/arcade vibe. Something you would see on your favorite pinball machine, or at your local roller rink.'
       }
     ]
+
+console.log(works[0].description)
 
 export default ImageList;
