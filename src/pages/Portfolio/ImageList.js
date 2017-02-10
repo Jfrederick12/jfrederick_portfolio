4f7c557b7a70ee606b7e07595c8ecb3323e4cfce
Jfrederick12 <jfrederickdesign@gmail.com>
2017-02-09 20:53:08 -0600

    update portfolio to include thumbnails

--- src/pages/Portfolio/ImageList.js
+++ src/pages/Portfolio/ImageList.js
@@ -92,33 +92,75 @@ const works = [
       { id: 1,
         name: "Moonphase Ranch",
         img: "https://d13yacurqjgara.cloudfront.net/users/1110044/screenshots/2648085/moonphase.jpg",
+        thumbnail: "https://github.com/Jfrederick12/React_Web_Portfolio/blob/master/public/thumbnails/moonphase_thumb.jpg?raw=true",
         description: "I spent a week on a ranch just outside of Austin, TX recording an upcoming record. Inspired by the acres of Texas hill country, a cozy air-stream trailer and free roaming cats, I created an illustration."
       },
       { id: 2,
         name: "CTA Illustration",
         img: "https://d13yacurqjgara.cloudfront.net/users/1110044/screenshots/2616895/ctatrain.jpg",
+        thumbnail: "https://github.com/Jfrederick12/React_Web_Portfolio/blob/master/public/thumbnails/cta_thumb.jpg?raw=true",
         description: "A series of illustrations commisioned by Chicago Red Eye to do a series of illustrations based on Chicago landmarks for their Social Media."
       },
       { id: 3,
         name: "Little Boy Jr",
         img: "https://d13yacurqjgara.cloudfront.net/users/1110044/screenshots/2633812/lbjdetail.jpg",
+        thumbnail: "https://github.com/Jfrederick12/React_Web_Portfolio/blob/master/public/thumbnails/lbj_thumb.jpg?raw=true",
         description: "Work in progress for album artwork."
       },
       { id: 4,
         name: "Music Box",
         img: "https://d13yacurqjgara.cloudfront.net/users/1110044/screenshots/2615073/music_box.jpg",
+        thumbnail: "https://github.com/Jfrederick12/React_Web_Portfolio/blob/master/public/thumbnails/music_box_thumb.jpg?raw=true",
         description: "A series of illustrations commisioned by Chicago Red Eye to do a series of illustrations based on Chicago landmarks for their Social Media."
       },
       { id: 5,
-        name: "Tuff Slang",
-        img: "https://d13yacurqjgara.cloudfront.net/users/1110044/screenshots/2599356/tuffslangvinyl.jpg",
+        name: "Cool Runnings",
+        img: "https://github.com/Jfrederick12/React_Web_Portfolio/blob/master/public/images/cool_runnings.jpg?raw=true",
+        thumbnail: "https://github.com/Jfrederick12/React_Web_Portfolio/blob/master/public/thumbnails/cool_runnings_thumb.jpg?raw=true",
         description: 'Album artwork for Tuff Slang release "Tuff Slang".'
       },
       { id: 6,
         name: "Blonde Ventura",
-        img: "https://d13yacurqjgara.cloudfront.net/users/1110044/screenshots/2596141/bv_screen.jpg",
+        img: "https://github.com/Jfrederick12/React_Web_Portfolio/blob/master/public/thumbnails/blonde_ventura_thumb.jpg?raw=true",
+        thumbnail: "https://github.com/Jfrederick12/React_Web_Portfolio/blob/master/public/thumbnails/blonde_ventura_thumb.jpg?raw=true",
         description: 'Type treatment for Los Angeles based Disco Pop group "Blonde Ventura". The idea was to go for a disco/arcade vibe. Something you would see on your favorite pinball machine, or at your local roller rink.'
-      }
+      },
+      { id: 7,
+        name: "Hobbit",
+        img: "https://github.com/Jfrederick12/React_Web_Portfolio/blob/master/public/images/hobbit.jpg?raw=true",
+        thumbnail: "https://github.com/Jfrederick12/React_Web_Portfolio/blob/master/public/thumbnails/hobbit_thumb.jpg?raw=true",
+        description: ""
+      },
+      { id: 8,
+        name: "Hobbit Portraits",
+        img: "https://github.com/Jfrederick12/React_Web_Portfolio/blob/master/public/images/hobbit_portraits.jpg?raw=true",
+        thumbnail: "https://github.com/Jfrederick12/React_Web_Portfolio/blob/master/public/thumbnails/hobbit_portraits_thumb.jpg?raw=true",
+        description: ""
+      },
+      { id: 9,
+        name: "SC Tour Poster",
+        img: "https://github.com/Jfrederick12/React_Web_Portfolio/blob/master/public/images/sc_canada.jpg?raw=true",
+        thumbnail: "https://github.com/Jfrederick12/React_Web_Portfolio/blob/master/public/thumbnails/sc_canada_thumb.jpg?raw=true",
+        description: ""
+      },
+      { id: 10,
+        name: "The Orwells NYE Poster",
+        img: "https://github.com/Jfrederick12/React_Web_Portfolio/blob/master/public/images/the_orwells.jpg?raw=true",
+        thumbnail: "https://github.com/Jfrederick12/React_Web_Portfolio/blob/master/public/thumbnails/the_orwells_thumb.jpg?raw=true",
+        description: ""
+      },
+      { id: 11,
+        name: "Logan Square",
+        img: "https://github.com/Jfrederick12/React_Web_Portfolio/blob/master/public/images/logan_square.jpg?raw=true",
+        thumbnail: "https://github.com/Jfrederick12/React_Web_Portfolio/blob/master/public/thumbnails/logan_thumb.jpg?raw=true",
+        description: ""
+      },
+      { id: 12,
+        name: "Seatd Mobile App",
+        img: "https://github.com/Jfrederick12/React_Web_Portfolio/blob/master/public/images/seatd.jpg?raw=true",
+        thumbnail: "https://github.com/Jfrederick12/React_Web_Portfolio/blob/master/public/images/seatd.jpg?raw=true",
+        description: ""
+      },
     ]
 
 console.log(works[0].description)
--- src/pages/Portfolio/ShowCard.js
+++ src/pages/Portfolio/ShowCard.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 const ShowCard = (props) => (
   <figure>
-    <img src={props.img} alt={props.name} height="225px" width="300px" />
+    <img src={props.thumbnail} alt={props.name} height="225px" width="300px" />
     <h1 className="">this</h1>
   </figure>
 )
@@ -11,7 +11,8 @@ const { string } = React.PropTypes
 
 ShowCard.propTypes = {
   img: string.isRequired,
-  name: string.isRequired
+  name: string.isRequired,
+  thumbnail: string.isRequired
 }
 
 export default ShowCard;
