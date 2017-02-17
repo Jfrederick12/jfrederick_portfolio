6eaae6038be270690023ce49da8eef4315ad5085
Jfrederick12 <jfrederickdesign@gmail.com>
2017-02-17 12:35:26 -0600

    fix social icon links, center properly

--- src/pages/Contact/ContactTray.js
+++ src/pages/Contact/ContactTray.js
@@ -8,8 +8,8 @@ const ContactTray = () => (
       <div className="contact-tray-container">
         <span id="image-header"><img src="https://github.com/Jfrederick12/React_Web_Portfolio/blob/buildout-1/public/images/thanks.png?raw=true" alt="thanks" width="60%"/></span>
         < PageNav />
+        < SocialLinkTray />
         <div className="social-container">
-          < SocialLinkTray />
         </div>
       </div>
 )
