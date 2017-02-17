66b7d750f37469e03de9bed792b15e4d67d9ea6d
Jfrederick12 <jfrederickdesign@gmail.com>
2017-02-17 12:21:45 -0600

    update work modal

--- src/pages/Portfolio/works.js
+++ src/pages/Portfolio/works.js
@@ -3,7 +3,11 @@ const works = [
         name: "Seatd Mobile App",
         img: "https://github.com/Jfrederick12/React_Web_Portfolio/blob/master/public/images/seatd.jpg?raw=true",
         thumbnail: "https://github.com/Jfrederick12/React_Web_Portfolio/blob/master/public/images/seatd.jpg?raw=true",
-        description: ""
+        description: "Client & business facing mobile app.",
+        involvement: "Focused on building out the functionality with AngularJS as well as the UI/UX. Worked on algorithms for the search feature that allows users to find businesses by location and service type.",
+        tech: "Angular, Ionic, Rails, SQL and Google Maps API.",
+        link: "link goes here",
+        linkImg: ""
       },
       { id: 1,
         name: "Moonphase Ranch",
@@ -15,25 +19,25 @@ const works = [
         name: "CTA Illustration",
         img: "https://d13yacurqjgara.cloudfront.net/users/1110044/screenshots/2616895/ctatrain.jpg",
         thumbnail: "https://github.com/Jfrederick12/React_Web_Portfolio/blob/master/public/thumbnails/cta_thumb.jpg?raw=true",
-        description: "A series of illustrations commisioned by Chicago Red Eye to do a series of illustrations based on Chicago landmarks for their Social Media."
+        description: "Part of a series of illustrations commisioned by Chicago Red Eye featuring Chicago landmarks for their Social Media platform."
       },
       { id: 3,
         name: "Little Boy Jr",
         img: "https://d13yacurqjgara.cloudfront.net/users/1110044/screenshots/2633812/lbjdetail.jpg",
         thumbnail: "https://github.com/Jfrederick12/React_Web_Portfolio/blob/master/public/thumbnails/lbj_thumb.jpg?raw=true",
-        description: "Work in progress for album artwork."
+        description: "Album artwork for Chicago band Little Boy Jr. Released on vinyl, cd and for digital distribution."
       },
       { id: 4,
         name: "Music Box",
         img: "https://d13yacurqjgara.cloudfront.net/users/1110044/screenshots/2615073/music_box.jpg",
         thumbnail: "https://github.com/Jfrederick12/React_Web_Portfolio/blob/master/public/thumbnails/music_box_thumb.jpg?raw=true",
-        description: "A series of illustrations commisioned by Chicago Red Eye to do a series of illustrations based on Chicago landmarks for their Social Media."
+        description: "Part of a series of illustrations commisioned by Chicago Red Eye featuring Chicago landmarks for their Social Media platform."
       },
       { id: 5,
         name: "Cool Runnings",
         img: "https://github.com/Jfrederick12/React_Web_Portfolio/blob/master/public/images/cool_runnings.jpg?raw=true",
         thumbnail: "https://github.com/Jfrederick12/React_Web_Portfolio/blob/master/public/thumbnails/cool_runnings_thumb.jpg?raw=true",
-        description: 'Album artwork for Tuff Slang release "Tuff Slang".'
+        description: 'Cool Runnings, the bands flagship since 2010. Handling everything from hauling a trailer full of instruments, containing road worn dudes, and making pit stops across fast food chains around North America.'
       },
       { id: 6,
         name: "Blonde Ventura",
@@ -45,31 +49,31 @@ const works = [
         name: "Hobbit",
         img: "https://github.com/Jfrederick12/React_Web_Portfolio/blob/master/public/images/hobbit.jpg?raw=true",
         thumbnail: "https://github.com/Jfrederick12/React_Web_Portfolio/blob/master/public/thumbnails/hobbit_thumb.jpg?raw=true",
-        description: ""
+        description: "Illustration inspired by the Hobbit. Bilbo and the Dwarves make their swift escape from Lonely Mountain while Smaug prepares to unleash a reign of fire on Laketown off in the distance."
       },
       { id: 8,
         name: "Hobbit Portraits",
         img: "https://github.com/Jfrederick12/React_Web_Portfolio/blob/master/public/images/hobbit_portraits.jpg?raw=true",
         thumbnail: "https://github.com/Jfrederick12/React_Web_Portfolio/blob/master/public/thumbnails/hobbit_portraits_thumb.jpg?raw=true",
-        description: ""
+        description: "Illustrations inspired by The Hobbit. I wanted to depict the Dwarves in a series of portraits, closely referencing the films portrayal."
       },
       { id: 9,
         name: "SC Tour Poster",
         img: "https://github.com/Jfrederick12/React_Web_Portfolio/blob/master/public/images/sc_canada.jpg?raw=true",
         thumbnail: "https://github.com/Jfrederick12/React_Web_Portfolio/blob/master/public/thumbnails/sc_canada_thumb.jpg?raw=true",
-        description: ""
+        description: "Canada Tour poster print for Chicago band Secret Colours"
       },
       { id: 10,
         name: "The Orwells NYE Poster",
         img: "https://github.com/Jfrederick12/React_Web_Portfolio/blob/master/public/images/the_orwells.jpg?raw=true",
         thumbnail: "https://github.com/Jfrederick12/React_Web_Portfolio/blob/master/public/thumbnails/the_orwells_thumb.jpg?raw=true",
-        description: ""
+        description: "Limited series of prints I was commisioned to illustrate for Atlantic Records artist, The Orwells at Lincoln Hall for NYE 2015"
       },
       { id: 11,
         name: "Logan Square",
         img: "https://github.com/Jfrederick12/React_Web_Portfolio/blob/master/public/images/logan_square.jpg?raw=true",
         thumbnail: "https://github.com/Jfrederick12/React_Web_Portfolio/blob/master/public/thumbnails/logan_thumb.jpg?raw=true",
-        description: ""
+        description: "Part of a series of illustrations commisioned by Chicago Red Eye featuring Chicago landmarks for their Social Media platform."
       }
     ]
 
