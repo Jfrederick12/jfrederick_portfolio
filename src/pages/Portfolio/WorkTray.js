062f04b09a2d9d7c880462fc0423ca8326b98d75
Jfrederick12 <jfrederickdesign@gmail.com>
2017-02-17 18:43:45 -0600

    add styling for media on portfolio page

--- src/pages/Portfolio/WorkTray.js
+++ src/pages/Portfolio/WorkTray.js
@@ -3,14 +3,9 @@ import { StickyContainer, Sticky } from 'react-sticky';
 import PageNav from '../../PageNav';
 import './Portfolio.css';
 
-const containerStyle = {
-  backgroundColor: '#173e43',
-  height: 3600
-}
-
 const WorkTray = () => (
-  <StickyContainer style={containerStyle}>
-    <Sticky bottomOffset={1} topOffset={-40} stickyStyle={{ marginTop: 40 }}>
+  <StickyContainer className="tray-container-main">
+    <Sticky bottomOffset={40} topOffset={-100} stickyStyle={{ marginTop: 100 }}>
     <div className="work-tray-container">
       <span id="image-header"><img src="https://github.com/Jfrederick12/React_Web_Portfolio/blob/buildout-1/public/images/work.png?raw=true" alt="work" width="60%"/></span>
       <h4>Recent Projects</h4>
