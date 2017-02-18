98a73a50442162560e27281367f9f9fafe899a4e
Jfrederick12 <jfrederickdesign@gmail.com>
2017-02-17 19:51:21 -0600

    fix typo

--- src/pages/Home/AboutDetails.js
+++ src/pages/Home/AboutDetails.js
@@ -12,7 +12,7 @@ class AboutDetails extends Component {
     return(
       <div className="about-container" style={containerStyle}>
         <span>
-          Coming from a place of design and illustration, I approach coding with a creative lens, always thinking about how I can lend my skills towards meaningful work. I’m incredibly determined and thoughtful about the projects I involve myself with and my intention is to create something that others will value. If i’ve learned anything by Roxette’s 1995 single, “Don’t Bore Us, Get to the Chorus!”, less is more.
+          Coming from a place of design and illustration, I approach coding through a creative lens, always thinking about how I can lend my skills towards meaningful work. I’m incredibly determined and thoughtful about the projects I involve myself with and my intention is to create something that others will value. If i’ve learned anything from Roxette’s 1995 single, “Don’t Bore Us, Get to the Chorus!”, less is more.
         </span>
         < SkillList />
       </div>
