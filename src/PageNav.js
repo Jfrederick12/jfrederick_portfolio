a5760633df2f58d23d2e81efcb6b594e6e5f8f33
Justin Frederick <jfrederickdesign@gmail.com>
2017-06-19 14:21:34 -0500

    add scrollable anchors

--- src/PageNav.js
+++ src/PageNav.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Scrollchor from 'react-scrollchor';
 
 const PageNav = () => (
   <div className="page-nav">
-    <span><a href="#about">about</a></span>
-    <span><a href="#work">work</a></span>
-    <span><a href="#contact">contact</a></span>
+    <span><Scrollchor to="#about" animate={{offset: 0, duration: 600}}>about</Scrollchor></span>
+    <span><Scrollchor to="#work" animate={{offset: 0, duration: 600}}>work</Scrollchor></span>
+    <span><Scrollchor to="#contact" animate={{offset: 0, duration: 600}}>contact</Scrollchor></span>
   </div>
 )
 
